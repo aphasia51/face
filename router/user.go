e76180d3ede0780857c731bb6f9f1b5b427cedfe
aphasia51 <xiahuas@outlook.com>
2023-04-25 21:04:00 +0800

    功能部分使用jwt中间件验证

--- router/user.go
+++ router/user.go
@@ -4,7 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 
-	"main/api"
+	"face/api"
+	"face/middleware"
 )
 
 func InitUserRouter(Router *gin.RouterGroup) {
@@ -15,5 +16,18 @@ func InitUserRouter(Router *gin.RouterGroup) {
 	{
 		UserRouter.GET("login", api.Login)
 		UserRouter.GET("register", api.Register)
+	}
+}
+
+func InitCutRouter(Router *gin.RouterGroup) {
+	CutRouter := Router.Group("func")
+
+	CutRouter.Use(middleware.AuthJWTMiddleware())
+
+	zap.S().Info("功能相关URL")
+
+	{
+		CutRouter.GET("commit", api.OneCut)
+		CutRouter.GET("push", api.Notice)
 	}
 }
