7638882956babd691cf8077763c9a90699def1c5
aphasia51 <xiahuas@outlook.com>
2023-04-25 13:50:06 +0800

    添加MYSQL配置结构体

---
+++ config/config.go
@@ -0,0 +1,9 @@
+package config
+
+type MySQLConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     string `mapstructure:"port"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+	Database string `mapstructure:"database"`
+}
