0e30e3d235d392d1338c0f7aff4c8351d66eee9e
aphasia51 <xiahuas@outlook.com>
2023-04-25 21:02:37 +0800

    设置跨域中间件

--- initialize/router.go
+++ initialize/router.go
@@ -3,13 +3,17 @@ package initialize
 import (
 	"net/http"
 
-	"main/router"
+	"face/middleware"
+	"face/router"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Routers() *gin.Engine {
 	Router := gin.Default()
+
+	Router.Use(middleware.Cors())
+
 	Router.GET("/health", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"Code":   http.StatusOK,
@@ -19,6 +23,7 @@ func Routers() *gin.Engine {
 
 	CutGroup := Router.Group("/v1/cut")
 	router.InitUserRouter(CutGroup)
+	router.InitCutRouter(CutGroup)
 
 	return Router
 }
